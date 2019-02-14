f1af8ca214720a499ede7522ebf50db14457e6aa
Hogan McDonald <hogan.developer@gmail.com>
2019-02-13 22:49:58 -0600

    adds theme and typography to layout component

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
+import { TypographyStyle, GoogleFont } from 'react-typography'
 
+import { theme } from '../styles/theme'
+import typography from '../styles/typography'
 import favicon from '../images/favicon.png'
+
+const Main = styled.main`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: ${({ theme }) => theme.colors.bg};
+`
 
 class Background extends Component {
   render() {
@@ -27,9 +37,13 @@ class Background extends Component {
             { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
           ]}
         >
-          <html lang="en" />
+          <html style={{ backgroundColor: 'purple' }} lang="en" />
         </Helmet>
-        <main>{children}</main>
+        <TypographyStyle typography={typography} />
+        <GoogleFont typography={typography} />
+        <ThemeProvider theme={theme}>
+          <Main>{children}</Main>
+        </ThemeProvider>
       </>
     )
   }
